046bfa6043def94dea7419d8fcc048bfeda20684
Ben Biddington <ben.biddington@gmail.com>
2012-07-30 21:22:31 +1200

    [WIP] emitting csv

--- mine.sh
+++ mine.sh
@@ -9,6 +9,8 @@ cd $the_git_repo
 number_of_files=`git ls-tree -r --name-only HEAD | wc -l`
 
 all_counts=`git log --name-status | grep -Ei "^[MA]\s+" | sort | uniq -c | sort -h | sed 's/^ *//g' | grep -Eo ^[0-9]+`
-
-echo "$all_counts"
-echo "There are <$number_of_files> files in total."
+csv=`echo "$all_counts" | tr '\n' ','`
+branch=`git branch`
+csv_file_name="csv/".$the_git_repo."_"_.`git branch`.".csv"
+echo $csv_file_name
+# echo "There are <$number_of_files> files in total."
