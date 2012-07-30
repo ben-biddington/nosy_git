53e262323aa6cb20bbc618273e09ae225cad2094
Ben Biddington <ben.biddington@gmail.com>
2012-07-30 19:09:30 +1200

    sort before uniq...

--- mine.sh
+++ mine.sh
@@ -6,6 +6,6 @@ echo "Mining source repo <$the_git_repo>"
 
 cd $the_git_repo
 
-count=`git log --name-status | grep -Ei "^[MA]\s+" | uniq -c | sort`
+count=`git log --name-status | grep -Ei "^[MA]\s+" | sort | uniq -c | sort -h`
 
 echo "There are <$count> files in the <$the_git_repo> repository"  
