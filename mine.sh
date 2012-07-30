a404596e3dc99a3bc3b53210438b87c848878f45
Ben Biddington <ben.biddington@gmail.com>
2012-07-30 19:00:45 +1200

    Added -c flag to uniq

--- mine.sh
+++ mine.sh
@@ -6,6 +6,6 @@ echo "Mining source repo <$the_git_repo>"
 
 cd $the_git_repo
 
-count=`git log --name-status | grep -Ei "^[MA]\s+" | uniq | wc -l`
+count=`git log --name-status | grep -Ei "^[MA]\s+" | uniq -c | sort`
 
 echo "There are <$count> files in the <$the_git_repo> repository"  
