d4479efd8dc7d5b9d0a3814bb253a2c68862e5d2
Ben Biddington <ben.biddington@gmail.com>
2012-07-30 18:40:00 +1200

    include added or modified log entries

--- mine.sh
+++ mine.sh
@@ -6,6 +6,6 @@ echo "Mining source repo <$the_git_repo>"
 
 cd $the_git_repo
 
-count=`git log --name-status | grep -Ei "^M\s+" | uniq | wc -l`
+count=`git log --name-status | grep -Ei "^[MA]\s+" | uniq | wc -l`
 
 echo "There are <$count> files in the repository"  
