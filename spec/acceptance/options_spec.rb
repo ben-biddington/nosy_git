06f0412e831b49610ffabc131aa3500b978b7b42
Ben Biddington <ben.biddington@gmail.com>
2012-09-29 21:13:17 +1200

    Added some notes on possible options

---
+++ spec/acceptance/options_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+require File.join File.dirname(__FILE__), "nosy_git_acceptance_test"
+
+describe "the output options you can supply" do
+  it "format might be nice, csv for example, pretty by default"
+  it "perhaps individual field selection, committer and date perhaps"
+end
