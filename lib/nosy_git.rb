7bfcd57505169a082d12c7ac571f86c8a8d46cc8
Ben Biddington <ben.biddington@gmail.com>
2012-09-29 21:29:04 +1200

    Nibbled away more of the executable.

--- lib/nosy_git.rb
+++ lib/nosy_git.rb
@@ -3,9 +3,26 @@ Dir.glob("#{File.dirname(__FILE__)}/nosy_git/**/*.rb").each {|f| require f }
 class NosyGit
   class << self
     def analyze file
+      verify file
+      header file 
+
       Revisions.for(file).each do |rev|
-        puts RevisionText.print file, rev 
+        UI.print RevisionText.print file, rev 
       end
+    end
+
+    private
+
+    def header(file)
+      require 'fileutils'
+
+      UI.print "(#{FileUtils.pwd})"
+      UI.print "Analyzing <#{file}>"
+    end
+
+    def verify(file)
+      UI.die "You have changes, aborting" if Git.has_changes?
+      UI.die "#{file} is not a file"  unless File.file? file
     end
   end
 end
