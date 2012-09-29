d826a18a31dcc36f1429a7caaf849ff61d831ea8
Ben Biddington <ben.biddington@gmail.com>
2012-09-29 21:48:29 +1200

    Refactor: moved tests about constraints to their own file

--- spec/acceptance/how_to_examine_a_file_spec.rb
+++ spec/acceptance/how_to_examine_a_file_spec.rb
@@ -1,38 +1,6 @@
 require "spec_helper"
 require File.join File.dirname(__FILE__), "nosy_git_acceptance_test"
 
-describe "constraints" do
-  include NosyGitAcceptanceTest
-
-  it "exits with status 1 if there are any changes" do
-    %x{ echo `date` >> README }
-    %x{ git commit -am "Jazz's bike seat" }
-    %x{ echo "Any staged change" >> README }
-    %x{ git commit -am "This one has been staged" }
-    %x{ echo "Any working tree change" >> README }
-
-    result = nosy_git "README"
-
-    then_it_aborts_because /You have changes/
-  end
-
-  it "exits with status 1 if the argument is not a file" do
-    dirname = "a_directory"
-    `mkdir #{dirname}`
-    `touch #{dirname}/.gitkeep`
-    `git add -A && git commit -m "Commiting a dir"`
-    
-    nosy_git "#{dirname}"
-
-    then_it_aborts_because /#{dirname} is not a file/
-  end
-
-  it "exits with status 1 if the argument is missing" do
-    result = nosy_git nil
-    then_it_aborts_because /No file/
-  end
-end
-
 describe "printing the rate of change of one file" do
   include NosyGitAcceptanceTest
   
