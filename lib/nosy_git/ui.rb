77804456d35ad5827d252df13a087b10a200c6da
Ben Biddington <ben.biddington@gmail.com>
2012-09-29 21:21:38 +1200

    Refactor: UI class => lib/

--- lib/nosy_git/ui.rb
+++ lib/nosy_git/ui.rb
@@ -1,3 +1,13 @@
+class UI
+  class << self
+    def die(because)
+      print because
+      exit 1
+    end
+    def print what; puts what; end
+  end
+end
+
 class RevisionText
   def self.print(the_file, rev)
     the_number_of_lines = Lines.for the_file, rev.number
