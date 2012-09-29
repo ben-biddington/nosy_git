2e36bd5ea78e0f1daa60b49fdd9708ea6b16a38d
Ben Biddington <ben.biddington@gmail.com>
2012-09-29 20:47:08 +1200

    Refactor: collected git-related classes

---
+++ lib/nosy_git/revisions.rb
@@ -0,0 +1,48 @@
+Revision = Struct.new(:timestamp, :number, :message, :author, :changes)
+Changes  = Struct.new(:added, :deleted)
+
+class Revisions
+  class << self
+    def for(file)
+      log_entries_for(file).map{|line| to_revision file, line }
+    end
+
+    def head(file)
+      fail "Not resetting without a file" if file.empty?
+      
+      `git reset HEAD #{file}`
+      `git checkout #{file}`
+    end
+
+    private
+
+    def log_entries_for file
+      %x{ git log --format="%ct %H auth='%an' %s" --follow -- #{file} }
+    end
+
+    def to_revision(file, line)
+      matches = line.match /([^\s]+)\s([^\s]+)\sauth='([^\"]+)'\s(.+)/
+
+      the_timestamp       = Time.at matches[1].strip.to_i
+      the_revision        = matches[2].strip
+      the_author          = matches[3].strip
+      the_commit_message  = matches[4].strip
+      
+      Revision.new(the_timestamp, the_revision, the_commit_message, the_author, changes(file, the_revision))
+    end
+
+    def changes(file, revision)
+      return Changes.new Lines.for(file, revision),0 unless has_parent? revision 
+      
+      matches = `git diff --numstat #{revision}^..#{revision} -- #{file}`.match /^([\d]+)\s+([\d]+)/
+
+      return Changes.new unless matches
+
+      Changes.new matches[1].strip.to_i, matches[2].strip.to_i
+    end
+
+    def has_parent?(revision)
+      `git cat-file -p #{revision}`.match /^parent/
+    end
+  end
+end
