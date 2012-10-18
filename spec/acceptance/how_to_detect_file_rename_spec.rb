79aab5d5f1e95cc311b95239f48fd868ce0ed0eb
Ben Biddington <ben.biddington@gmail.com>
2012-10-18 20:58:32 +1300

    can get the new file name for any commit

--- spec/acceptance/how_to_detect_file_rename_spec.rb
+++ spec/acceptance/how_to_detect_file_rename_spec.rb
@@ -1,18 +1,19 @@
 require "spec_helper"
 require File.join File.dirname(__FILE__), "nosy_git_acceptance_test"
 
-class Renamed
+class Rename
   class << self
     def for(file, revision)
-      result = `git diff-index -M HEAD^`
-      match = result.match /#{file}\s+(.+)$/
+      result = `git diff -M #{revision}^..#{revision}`
+
+      match = result.match /rename to\s+(.+)$/i
       the_new_name = match[1] if match
       the_new_name
     end
   end
 end
 
-describe "detecting file renames using `git diff-index`" do
+describe "detecting file renames using `git diff`" do
   include NosyGitAcceptanceTest
 
   it "can get the new name of a file when HEAD is a pure rename" do
@@ -21,8 +22,23 @@ describe "detecting file renames using `git diff-index`" do
       git commit -m "Renamed the README"
     }
     
-    result = Renamed.for "README", "HEAD"
+    result = Rename.for("README", "HEAD").must === "README RENAMED"
+  end
+
+  it "can get the new name for any commit, i.e., a commit that is not the current HEAD" do
+    new_name = "README RENAMED"
+
+    `echo "#1" >> README         && git commit -am "COMMIT #1"`
+    `git mv README "#{new_name}" && git commit -am "COMMIT #2 (RENAME)"`
+    `echo "#3" >> "#{new_name}"  && git commit -am "COMMIT #3"`
+
+    Rename.for("README", "HEAD^").must === new_name
+  end
+
+  it "if the commit is not a rename it returns nil" do
+    `echo "#1" >> README && git commit -am "COMMIT #1"`
+    `echo "#2" >> README && git commit -am "COMMIT #2"`
     
-    result.must === "README RENAMED"
+    Rename.for("README", "HEAD^").must be_nil
   end
 end
