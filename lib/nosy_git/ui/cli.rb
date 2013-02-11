df9770d1dc40f50315ce7b728ab8c4a22c643fd5
Ben Biddington <ben.biddington@gmail.com>
2013-02-12 12:01:16 +1300

    Added a CSV view

--- lib/nosy_git/ui/cli.rb
+++ lib/nosy_git/ui/cli.rb
@@ -30,6 +30,32 @@ class Pretty
   end
 end
 
+class CSV
+  def initialize(file)
+    @file = file
+    print_header
+  end
+
+  def print(revision)
+    UI.print( 
+      "#{revision.timestamp}," + 
+      "#{revision.changes.added}," + 
+      "#{revision.changes.deleted}," + 
+      "#{revision.changes.net_added}"
+    )
+  end
+
+  def die(because)
+    UI.die because
+  end
+
+  private
+
+  def print_header
+    UI.print "timestamp,added,deleted,change"
+  end
+end
+
 class UI
   class << self
     def die(because)
