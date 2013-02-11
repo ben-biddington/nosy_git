b8e249eda8208fbf48f4d1aefed3ef5fb0d68cb2
Ben Biddington <ben.biddington@gmail.com>
2013-02-12 12:14:14 +1300

    class => file

---
+++ lib/nosy_git/ui/csv.rb
@@ -0,0 +1,25 @@
+class CSV
+  def initialize(file)
+    @file = file
+    print_header
+  end
+
+  def print(revision)
+    UI.print( 
+      "#{revision.timestamp}," + 
+      "#{revision.changes.added}," + 
+      "#{revision.changes.deleted}," + 
+      "#{revision.changes.net_added}"
+    )
+  end
+
+  def die(because)
+    UI.die because
+  end
+
+  private
+
+  def print_header
+    UI.print "timestamp,added,deleted,change"
+  end
+end
