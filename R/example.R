46829fa52f76d4674a7c1c404ce8d16574069588
Ben Biddington <ben.biddington@gmail.com>
2013-02-12 16:56:29 +1300

    Titles and size

--- R/example.R
+++ R/example.R
@@ -6,7 +6,7 @@ title       = args[3]
 data = read.table(www, head=T, sep=",")
 reversed = data[order(nrow(data):1),]
 
-png(output_file)
+png(output_file, width=800, height=600)
 frame()
-plot(reversed$line_count, type="l", main=paste(title), ylab="No. lines", xlab="Time")
+plot(reversed$line_count, type="l", main=paste(title), ylab="Number of lines", xlab="Commits")
 dev.off()
